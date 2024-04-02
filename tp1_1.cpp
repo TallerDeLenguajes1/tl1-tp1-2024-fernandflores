1af55b3c641b093fb33314e523eba6af44d57896
Cesar Fernando Flores <fernandofloresfacet@gmail.com>
2024-04-02 02:08:06 -0300

    agregado de punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-
+    int num=5, tam;
+    int *puntero;
+    puntero=&num;
+    tam= sizeof(num);
     cout << "hola mundo" <<endl;
+    cout << "contenido del puntero: " << *puntero <<endl;
+    cout << "direccion de memoria almacenada por el puntero: " << puntero <<endl;
+    cout << "direccion de memoria de la variable: " << &num <<endl;
+    cout << "direccion de memoria del puntero: " << &puntero <<endl;
+    cout << "tamaño de memoria utulizado por la variable: " << tam <<endl;
         
     return 0;
 }
